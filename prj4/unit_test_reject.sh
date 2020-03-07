4beb393db0aa4b05809c61eabeeea04ff0c834e0
DLJ42 <26966954+DLJ42@users.noreply.github.com>
2020-03-07 13:35:06 -0500

    prj3 updates

--- prj4/unit_test_reject.sh
+++
@@ -1,8 +0,0 @@
-cd ~/Documents/GitHub/Compilers-COP4620/prj2/reject_test/
-alias ls='ls'
-for i in `ls`;
-do
-python ~/Documents/GitHub/Compilers-COP4620/prj2/parser.py $i;
-done;
-rm tokens.txt
-python ~/Documents/GitHub/Compilers-COP4620/prj2/parser.py
